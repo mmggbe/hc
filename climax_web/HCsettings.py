eed6d495b8711de30fd2d3ca30fc077098605b7a
«Marc» <mage.gerin@gmail.com>
2017-08-08 18:08:50 +0200

    Fix typo

--- climax_web/HCsettings.py
+++ climax_web/HCsettings.py
@@ -3,7 +3,7 @@
 
 class Notifier:
     __conf = {
-        "CLIENT_ID" : "tECLyAst04Lkfv5K_VgftPkc7Swa",
+        "CLIENT_ID" : "tECLyAst04Lkfv5K_VgftPkc7S",
         "CLIENT_SECRET" : "Y4oy9qhXJs3BEDbcgVLyzoJ4DMwa"
     }
     @staticmethod
