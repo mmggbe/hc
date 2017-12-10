82a6169d6e29d35ee976642a923cc6a99e63787e
Home Control <hc@Horus.ovh>
2017-12-10 21:28:48 +0100

    Camera improvements

--- climax_web/camera/urls.py
+++ climax_web/camera/urls.py
@@ -4,10 +4,11 @@ from . import views
 urlpatterns= [
         url(r'^$', views.cameraList, name='cameraList'),
         url(r'^cameraAdd/', views.cameraAdd),
+        url(r'^cameraEdit/(?P<pk>\d+)$', views.cameraEdit, name='cameraEdit'),
         url(r'^cameraSettings/', views.cameraSettings),
         url(r'^saveCamera/', views.saveCamera),
         url(r'^cameraDelete/(?P<pk>\d+)$', views.cameraDelete, name='cameraDelete'),
-        url(r'^cameraEdit/(?P<pk>\d+)$', views.cameraEdit, name='cameraEdit'),
+        url(r'^cameraTest/(?P<pk>\d+)$', views.cameraTest, name='cameraTest'),
         url(r'^cameraRT/(?P<pk>\d+)$', views.cameraRT, name='cameraRT'),
         url(r'^cameraRTStop/(?P<pk>\d+)$', views.cameraRTStop, name='cameraRTStop'),
         url(r'^saveAction/', views.saveAction),
