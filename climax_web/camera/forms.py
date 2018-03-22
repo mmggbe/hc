072ff354d16242ce0413f335592c45df63ca0ab6
«Marc» <mage.gerin@gmail.com>
2018-03-22 21:42:27 +0100

    Add missing forms.py for Camera

---
+++ climax_web/camera/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.core.validators import RegexValidator
+
+from .models import camera
+
+
+class cameraEditForm(forms.ModelForm):
+    
+    CameraMac = forms.CharField(label='MAC address',validators=[RegexValidator(regex=r'^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$', message='MAC addrss should be XX:XX:XX:XX:XX:XX')])
+    description = forms.CharField()
+
+    def clean_CameraMac(self):
+        mac_passed = self.cleaned_data.get("CameraMac")
+        
+        # to be improved : allows one users has 2 time the same MAC
+        cam_list = camera.objects.filter(CameraMac=mac_passed).exclude(user = self.cur_usr)
+        if cam_list:
+            raise forms.ValidationError("Sorry, the MAC address you gave is already in use")
+        
+        return mac_passed
+
+    class Meta:
+        model = camera
+        fields = ('CameraMac','description',)    
+        
+        
+    def __init__(self, user, *args, **kwargs):
+        super(cameraEditForm, self).__init__(*args, **kwargs)
+        self.cur_usr= user.id
